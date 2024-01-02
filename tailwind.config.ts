7c1b0b774643d15174a8adffa126e5cab518d9ee
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-01-02 20:41:33 +0800

    added font family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      fontFamily: {
+        body: ["var(--font-nunito-sans"],
+        display: ["var(--font-nunito"],
+        }
     },
   },
   plugins: [],
