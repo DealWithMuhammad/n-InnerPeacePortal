7874ed95bc8c3532a0faa8125d75f6f51f818be5
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-01-04 22:16:33 +0800

    testing commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className=" text-5xl font-display text-red-500 ">Hello World</h1>
+      <h1 className=" text-5xl font-display text-red-500 ">Hllo World</h1>
     </>
   );
 }
