cd1151aab320c558cf18e7c758e9469d8e7c6130
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-01-10 16:55:06 +0800

    feat: Added Hero and a section refractor called Bounded  Added styling in elements uisng tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@ import { Metadata } from "next";
 import { SliceZone } from "@prismicio/react";
 
 import { createClient } from "@/prismicio";
-// import { components } from "@/slices";
+import { components } from "@/slices";
 
 export default async function Page() {
   const client = createClient();
   const page = await client.getSingle("homepage");
 
-  return <div>Homepage</div>;
-  // return <SliceZone slices={page.data.slices} components={components} />;
+  return <SliceZone slices={page.data.slices} components={components} />;
 }
 
 export async function generateMetadata(): Promise<Metadata> {
---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,63 @@
+import { Content } from "@prismicio/client";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import Bounded from "@/components/Bounded";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    // can also use section instead of bounded
+    <Bounded
+      className="px-4  py-10 md:py-14 md:px-6 lg:py-16"
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      {/* <div className="mx-auto w-full max-w-6xl"> First way : To add manually, Second way to add Bounded*/}
+      <div className="grid grid-cols-1 place-items-center text-center">
+        <PrismicRichText
+          field={slice.primary.heading}
+          components={{
+            heading1: ({ children }) => (
+              <h1 className="text-5xl  md:text-7xl font-bold leading-tight tracking-tighter font-display text-slate-700">
+                {children}
+              </h1>
+            ),
+          }}
+        />
+
+        <PrismicRichText
+          field={slice.primary.body}
+          components={{
+            paragraph: ({ children }) => (
+              <p className="text-2xl text-center text-slate-600 mb-4 md:mb-8 max-w-md font-normal leading-10 font-body">
+                {children}
+              </p>
+            ),
+          }}
+        />
+
+        <PrismicNextLink
+          field={slice.primary.button_link}
+          className="block w-fit bg-cyan-700 hover:bg-cyan-800 transition-color duration-200 ease-in-out py-3 px-12 rounded-full font-display text-white font-bold text-base mb-8 md:mb-10 tracking-wider"
+        >
+          <>{slice.primary.button_text}</>
+        </PrismicNextLink>
+
+        <PrismicNextImage
+          field={slice.primary.image}
+          className="drop-shadow-xl max-w-4xl w-full"
+        />
+      </div>
+      {/* </div> */}
+    </Bounded>
+  );
+};
+
+export default Hero;
