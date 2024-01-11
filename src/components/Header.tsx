219e75c1909ec46cbf833014220a0e6fe90b1030
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-01-11 16:49:41 +0800

    feat: Styled Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { createClient } from "@/prismicio";
 import { PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
+import Bounded from "./Bounded";
+import Logo from "./Logo";
 
 export default async function Header() {
   const client = createClient();
@@ -8,17 +10,23 @@ export default async function Header() {
   const settings = await client.getSingle("settings");
 
   return (
-    <header>
-      <Link href="/">{settings.data.site_title}</Link>
-      <nav>
-        <ul>
-          {settings.data.navigation.map(({ link, label }) => (
-            <li key={label}>
-              <PrismicNextLink field={link}>{label}</PrismicNextLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <Bounded as="header" className="py-4 md:py-6 lg:py-8 ">
+      <div className="flex gap-4 items-center justify-between sm:flex-row flex-col">
+        <Link href="/">
+          <Logo />
+        </Link>
+        <nav>
+          <ul className="flex">
+            {settings.data.navigation.map(({ link, label }) => (
+              <li key={label}>
+                <PrismicNextLink className="p-3" field={link}>
+                  {label}
+                </PrismicNextLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </Bounded>
   );
 }
