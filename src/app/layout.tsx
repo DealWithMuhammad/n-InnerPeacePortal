51c23b9c71b298be93cf7d2e71251df371606f80
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-01-14 16:49:40 +0800

    feat: Add Prismic Preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,34 @@
-import type { Metadata, ResolvingMetadata } from "next";
-import { Nunito, Nunito_Sans } from "next/font/google";
-import clsx from "clsx";
 import "./globals.css";
-import { createClient } from "@/prismicio";
+import type { Metadata } from "next";
+import clsx from "clsx";
+import { Nunito, Nunito_Sans } from "next/font/google";
+import { createClient, repositoryName } from "@/prismicio";
+import { PrismicPreview } from "@prismicio/next";
+
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 const nunito = Nunito({
   subsets: ["latin"],
-  variable: "--font-nunito",
   display: "swap",
+  variable: "--font-nunito",
 });
 
 const nunitoSans = Nunito_Sans({
   subsets: ["latin"],
-  variable: "--font-nunito-sans",
   display: "swap",
+  variable: "--font-nunito-sans",
 });
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
 
   const settings = await client.getSingle("settings");
+
   return {
-    title: settings.data.site_title || "Flowrise Fallback",
+    title: settings.data.site_title || "Flowrise",
     description:
-      settings.data.meta_description || "Flowrise is the relaxing app for you",
+      settings.data.meta_description || "Flowrise is the relaxing app for you.",
     openGraph: {
       images: [settings.data.og_image.url || ""],
     },
@@ -43,6 +46,8 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <div className="fixed bg-gradient-to-tr from-emerald-50 to-cyan-50 z-[-1] inset-0 opacity-50" />
+        <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
   );
