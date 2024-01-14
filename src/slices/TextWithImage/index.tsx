3b41c2b908bc9a9a3ce1c9613eaa48a36ab431dd
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-01-14 15:33:51 +0800

    feat: Styled TextWithImage slice

--- src/slices/TextWithImage/index.tsx
+++ src/slices/TextWithImage/index.tsx
@@ -1,5 +1,25 @@
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import {
+  SliceComponentProps,
+  JSXMapSerializer,
+  PrismicRichText,
+} from "@prismicio/react";
+import { PrismicNextImage } from "@prismicio/next";
+import clsx from "clsx";
+
+import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
+
+const components: JSXMapSerializer = {
+  heading2: ({ children }) => (
+    <Heading as="h2" size="lg" className="">
+      {children}
+    </Heading>
+  ),
+  paragraph: ({ children }) => (
+    <p className="max-w-md text-lg font-body text-slate-600">{children}</p>
+  ),
+};
 
 /**
  * Props for `TextWithImage`.
@@ -12,13 +32,28 @@ export type TextWithImageProps =
  */
 const TextWithImage = ({ slice }: TextWithImageProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for text_with_image (variation: {slice.variation})
-      Slices
-    </section>
+      <div className="grid gap-8 md:grid-cols-2 place-items-center">
+        <PrismicNextImage
+          field={slice.primary.image}
+          className={clsx(
+            "rounded-lg",
+            slice.variation === "imageRight" && "md:order-2"
+          )}
+        />
+
+        <div className="grid gap-4">
+          <PrismicRichText
+            field={slice.primary.heading}
+            components={components}
+          />
+          <PrismicRichText field={slice.primary.body} components={components} />
+        </div>
+      </div>
+    </Bounded>
   );
 };
 
